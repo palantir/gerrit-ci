c346a09e8146958afc98c73610672af056802583
Andrew Mass <amass@palantir.com>
2015-07-01 19:39:55 -0700

    Improve look-and-feel of project settings screen.
    Change-Id: I6d3345715451e5c9d6302289a6326652670341f5

--- src/main/java/com/palantir/gerrit/gerritci/ui/client/ProjectSettingsScreen.java
+++ src/main/java/com/palantir/gerrit/gerritci/ui/client/ProjectSettingsScreen.java
@@ -10,13 +10,17 @@ import com.google.gerrit.plugin.client.screen.Screen;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.dom.client.ParagraphElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasAlignment;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -102,7 +106,7 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
                 publishCommand = new TextBox();
                 timeoutEnabled = new CheckBox("Disabled");
                 timeoutMinutes = new TextBox();
-                saveButton = new Button("Save");
+                saveButton = new Button("Save & Update");
 
                 // header
                 HeadingElement header = Document.get().createHElement(1);
@@ -116,6 +120,7 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
 
                 // verifyJobEnabled
                 verifyJobEnabled.setValue(false);
+                verifyJobEnabled.setEnabled(false);
                 verifyJobEnabled.addClickHandler(new ClickHandler() {
 
                     @Override
@@ -126,14 +131,37 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
                     }
                 });
                 verticalPanel.add(verifyJobEnabled);
+                ParagraphElement verifyJobEnabledDescription = Document.get().createPElement();
+                verifyJobEnabledDescription.setClassName("description");
+                verifyJobEnabledDescription
+                    .setInnerText("Check this checkbox to enable verify jobs for your project");
+                verticalPanel.add(HTML.wrap(verifyJobEnabledDescription));
 
                 // verifyBranchRegex
+                ParagraphElement verifyBranchRegexLabel = Document.get().createPElement();
+                verifyBranchRegexLabel.setInnerText("Regex for branches to verify");
+                verifyBranchRegexLabel.setClassName("label");
+                verticalPanel.add(HTML.wrap(verifyBranchRegexLabel));
                 verifyBranchRegex.setText(".*");
                 verticalPanel.add(verifyBranchRegex);
+                ParagraphElement verifyBranchRegexDescription = Document.get().createPElement();
+                verifyBranchRegexDescription.setClassName("description");
+                verifyBranchRegexDescription
+                    .setInnerText("Branches matching this regex will have the verify job command run on them by Jenkins");
+                verticalPanel.add(HTML.wrap(verifyBranchRegexDescription));
 
                 // verifyCommand
+                ParagraphElement verifyCommandLabel = Document.get().createPElement();
+                verifyCommandLabel.setInnerText("Command to run for a verify job");
+                verifyCommandLabel.setClassName("label");
+                verticalPanel.add(HTML.wrap(verifyCommandLabel));
                 verifyCommand.setText("./scripts/verify.sh");
                 verticalPanel.add(verifyCommand);
+                ParagraphElement verifyCommandDescription = Document.get().createPElement();
+                verifyCommandDescription.setClassName("description");
+                verifyCommandDescription
+                    .setInnerText("Simple command or a shell script to run for verify jobs");
+                verticalPanel.add(HTML.wrap(verifyCommandDescription));
 
                 // publishHeader
                 HeadingElement publishHeader = Document.get().createHElement(2);
@@ -142,6 +170,7 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
 
                 // publishJobEnabled
                 publishJobEnabled.setValue(false);
+                publishJobEnabled.setEnabled(false);
                 publishJobEnabled.addClickHandler(new ClickHandler() {
 
                     @Override
@@ -152,14 +181,37 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
                     }
                 });
                 verticalPanel.add(publishJobEnabled);
+                ParagraphElement publishJobEnabledDescription = Document.get().createPElement();
+                publishJobEnabledDescription.setClassName("description");
+                publishJobEnabledDescription
+                    .setInnerText("Check this checkbox to enable publish jobs for your project");
+                verticalPanel.add(HTML.wrap(publishJobEnabledDescription));
 
                 // publishBranchRegex
+                ParagraphElement publishBranchRegexLabel = Document.get().createPElement();
+                publishBranchRegexLabel.setInnerText("Regex for branches to publish");
+                publishBranchRegexLabel.setClassName("label");
+                verticalPanel.add(HTML.wrap(publishBranchRegexLabel));
                 publishBranchRegex.setText("refs/heads/(develop|master)");
                 verticalPanel.add(publishBranchRegex);
+                ParagraphElement publishBranchRegexDescription = Document.get().createPElement();
+                publishBranchRegexDescription.setClassName("description");
+                publishBranchRegexDescription
+                    .setInnerText("Branches matching this regex will have the publish job command run on them by Jenkins");
+                verticalPanel.add(HTML.wrap(publishBranchRegexDescription));
 
                 // publishCommand
+                ParagraphElement publishCommandLabel = Document.get().createPElement();
+                publishCommandLabel.setInnerText("Command to run for a publish job");
+                publishCommandLabel.setClassName("label");
+                verticalPanel.add(HTML.wrap(publishCommandLabel));
                 publishCommand.setText("./scripts/publish.sh");
                 verticalPanel.add(publishCommand);
+                ParagraphElement publishCommandDescription = Document.get().createPElement();
+                publishCommandDescription.setClassName("description");
+                publishCommandDescription
+                    .setInnerText("Simple command or a shell script to run for publish jobs");
+                verticalPanel.add(HTML.wrap(publishCommandDescription));
 
                 // generalHeader
                 HeadingElement generalHeader = Document.get().createHElement(2);
@@ -167,12 +219,14 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
                 verticalPanel.add(HTML.wrap(generalHeader));
 
                 // timeoutHeader
-                HeadingElement timeoutHeader = Document.get().createHElement(3);
+                HeadingElement timeoutHeader = Document.get().createHElement(2);
                 timeoutHeader.setInnerText("Timeouts");
+                timeoutHeader.setClassName("subsection");
                 verticalPanel.add(HTML.wrap(timeoutHeader));
 
                 // timeoutEnabled
                 timeoutEnabled.setValue(false);
+                timeoutEnabled.setEnabled(false);
                 timeoutEnabled.addClickHandler(new ClickHandler() {
 
                     @Override
@@ -183,10 +237,24 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
                     }
                 });
                 verticalPanel.add(timeoutEnabled);
+                ParagraphElement timeoutEnabledDescription = Document.get().createPElement();
+                timeoutEnabledDescription.setClassName("description");
+                timeoutEnabledDescription
+                    .setInnerText("Check this checkbox to enable job timeouts for your project");
+                verticalPanel.add(HTML.wrap(timeoutEnabledDescription));
 
                 // timeoutMinutes
-                timeoutMinutes.setText("15");
+                ParagraphElement timeoutMinutesLabel = Document.get().createPElement();
+                timeoutMinutesLabel.setInnerText("Timeout minutes");
+                timeoutMinutesLabel.setClassName("label");
+                verticalPanel.add(HTML.wrap(timeoutMinutesLabel));
+                timeoutMinutes.setText("30");
                 verticalPanel.add(timeoutMinutes);
+                ParagraphElement timeoutMinutesDescription = Document.get().createPElement();
+                timeoutMinutesDescription.setClassName("description");
+                timeoutMinutesDescription
+                    .setInnerText("Jenkins will wait this many minutes before terminating the job's run");
+                verticalPanel.add(HTML.wrap(timeoutMinutesDescription));
 
                 // saveButton
                 saveButton.addClickHandler(new ClickHandler() {
@@ -218,11 +286,14 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
 
                                 @Override
                                 public void onSuccess(JavaScriptObject result) {
-                                    // TODO: Handle this situation
+                                    alertWidget("Jenkins Server Response",
+                                                "Jenkins has been updated successfully with your settings")
+                                        .center();
                                 }
                             });
                     }
                 });
+                saveButton.setEnabled(false);
                 verticalPanel.add(saveButton);
 
                 updateWidgetEnablity();
@@ -282,6 +353,10 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
                                 timeoutMinutes.setText(String.valueOf(timeoutMinutesInteger));
                             }
 
+                            verifyJobEnabled.setEnabled(true);
+                            publishJobEnabled.setEnabled(true);
+                            timeoutEnabled.setEnabled(true);
+                            saveButton.setEnabled(true);
                             updateWidgetEnablity();
                         }
                     });
@@ -301,7 +376,42 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
         publishBranchRegex.setEnabled(publishJobEnabled.getValue());
         publishCommand.setEnabled(publishJobEnabled.getValue());
 
-        timeoutEnabled.setEnabled(verifyJobEnabled.getValue() || publishJobEnabled.getValue());
         timeoutMinutes.setEnabled(timeoutEnabled.getValue());
+    }
+
+    /**
+     * Creates a DialogBox object with the specified header and content and returns it to be
+     * displayed.
+     *
+     * @param header Title for the alert dialog
+     * @param content Inner message content to display
+     * @return A DialogBox object configured with the specified strings
+     */
+    public static DialogBox alertWidget(final String header, final String content) {
+        final DialogBox dialogBox = new DialogBox();
+        final VerticalPanel verticalPanel = new VerticalPanel();
+
+        final Label emptyLabel = new Label("");
+        emptyLabel.setSize("auto", "25px");
+
+        dialogBox.setText(header);
+
+        verticalPanel.add(emptyLabel);
+        verticalPanel.add(new Label(content));
+        verticalPanel.add(emptyLabel);
+
+        final Button buttonClose = new Button("Close", new ClickHandler() {
+
+            @Override
+            public void onClick(final ClickEvent event) {
+                dialogBox.hide();
+            }
+        });
+        buttonClose.setWidth("100px");
+        verticalPanel.add(buttonClose);
+        verticalPanel.setCellHorizontalAlignment(buttonClose, HasAlignment.ALIGN_CENTER);
+
+        dialogBox.add(verticalPanel);
+        return dialogBox;
     }
 }
