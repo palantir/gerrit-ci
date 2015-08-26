cbc8939d12da1092cda68870107f4bdf1c0647b2
Camus Ma <camusm@palantir.com>
2015-08-26 10:42:59 -0700

    Rename plugin entry point to gerritciplugin.
    ProjectSettingScreen is now an obsolete and misleading name for
    the class since it no longer displays a screen but redirects to
    either the project screen or admin screen.

--- src/main/java/com/palantir/gerrit/gerritci/ui/client/GerritCiPlugin.java
+++ src/main/java/com/palantir/gerrit/gerritci/ui/client/GerritCiPlugin.java
@@ -3,12 +3,12 @@
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
-// 
+//
 //       http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //   Unless required by applicable law or agreed to in writing, software
 //   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 package com.palantir.gerrit.gerritci.ui.client;
@@ -23,15 +23,15 @@ import com.google.gwt.user.client.ui.Image;
  * This class shows the Gerrit-CI settings screen for a particular project to users with the proper
  * permissions.
  */
-public class ProjectSettingsScreen extends PluginEntryPoint {
+public class GerritCiPlugin extends PluginEntryPoint {
 
     public static final Resources RESOURCES = GWT.create(Resources.class);
-    public static final HTMLPanel buttonsPanel = new HTMLPanel(ProjectSettingsScreen.RESOURCES.buttonsPanel().getText());
-    public static final HTMLPanel cronToggle = new HTMLPanel(ProjectSettingsScreen.RESOURCES.cron().getText());
-    public static final HTMLPanel cronPanel = new HTMLPanel(ProjectSettingsScreen.RESOURCES.cronJobPanel().getText());
-    public static final HTMLPanel publishJobPanel = new HTMLPanel(ProjectSettingsScreen.RESOURCES.publishJobPanel().getText());
-    public static final HTMLPanel verifyJobPanel = new HTMLPanel(ProjectSettingsScreen.RESOURCES.verifyJobPanel().getText());
-    public static final Image cronToggleButton = new Image(ProjectSettingsScreen.RESOURCES.info());
+    public static final HTMLPanel buttonsPanel = new HTMLPanel(GerritCiPlugin.RESOURCES.buttonsPanel().getText());
+    public static final HTMLPanel cronToggle = new HTMLPanel(GerritCiPlugin.RESOURCES.cron().getText());
+    public static final HTMLPanel cronPanel = new HTMLPanel(GerritCiPlugin.RESOURCES.cronJobPanel().getText());
+    public static final HTMLPanel publishJobPanel = new HTMLPanel(GerritCiPlugin.RESOURCES.publishJobPanel().getText());
+    public static final HTMLPanel verifyJobPanel = new HTMLPanel(GerritCiPlugin.RESOURCES.verifyJobPanel().getText());
+    public static final Image cronToggleButton = new Image(GerritCiPlugin.RESOURCES.info());
 
     @Override
     public void onPluginLoad() {
@@ -42,6 +42,6 @@ public class ProjectSettingsScreen extends PluginEntryPoint {
          * The regex will match all strings following "projects/". This is necessary because project
          * names can contain slashes.
          */
-        Plugin.get().screenRegex("projects/.+", new ProjectConfigurationScreen.Factory());
+        Plugin.get().screenRegex("projects/.+", new ProjectConfigurationScreen.Factory());;
     }
 }
